f422ece2c21c88339b78ea18915fb0b7e3ad3423
Habit <habit@SK-20220708ZSYO>
2024-10-09 22:19:26 +0800

    Finishing Homework1&2

---
+++ test_pcl.cpp
@@ -0,0 +1,10 @@
+#include <pcl/point_cloud.h>
+
+#include <pcl/point_types.h>
+#include <iostream>
+
+int main() {
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    std::cout << "PCL is installed and working!" << std::endl;
+    return 0;
+}
